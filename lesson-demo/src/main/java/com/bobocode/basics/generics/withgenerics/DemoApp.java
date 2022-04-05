81f8b7593a0335eda296a889e87db15899f2a437
Ihor Zaporozhtsev <i.zaporozhtsev@gmail.com>
2022-04-05 11:01:21 +0300

    example error in compile time

--- lesson-demo/src/main/java/com/bobocode/basics/generics/withgenerics/DemoApp.java
+++ lesson-demo/src/main/java/com/bobocode/basics/generics/withgenerics/DemoApp.java
@@ -3,6 +3,7 @@ package com.bobocode.basics.generics.withgenerics;
 public class DemoApp {
     public static void main(String[] args) {
         Container<Integer> integerContainer = new Container<Integer>(25);
+        //integerContainer.setValue("Andriy"); // про цю помилку бачимо на етапі компіляції
         int val = integerContainer.getValue();
         System.out.println(val);
     }
