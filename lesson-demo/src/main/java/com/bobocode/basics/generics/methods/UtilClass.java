e2cb75b5ce41bdd2ee0828a0c54ee28bccec7fb3
Ihor Zaporozhtsev <i.zaporozhtsev@gmail.com>
2022-04-07 10:08:24 +0300

    generics wildcard and return type

--- lesson-demo/src/main/java/com/bobocode/basics/generics/methods/UtilClass.java
+++ lesson-demo/src/main/java/com/bobocode/basics/generics/methods/UtilClass.java
@@ -20,4 +20,27 @@ public class UtilClass {
     static void printAllWildCard(List<? extends BaseEntity> list){
         list.forEach(System.out::println);
     }
+
+    //якщо в рамках метода Тип використовується тоді його треба і використовувати в параметрах
+    //це приклад з помилкою
+    static <T> T usual(List<T> list){
+        T variable;
+        variable = list.get(0);
+        return variable;
+    }
+    // приклад з помилкою
+    static <T> T wrongExample(List<? extends BaseEntity> list){
+        T variable = null;
+        //variable = list.get(0); // не зрозуміло який тим тому що знак питання
+        return variable;
+    }
+
+    //це приклад з помилкою
+    //wildcard в параметрах метода використовуємо тільки коли немає Типу в рамках методу. Якщо в рамках метода Тип використовується то wildcard не використовуємо
+    static <T extends BaseEntity> T wrongExampleSecond(List<T/* extends BaseEntity*/> list){
+        T variable = null;
+        //variable = list.get(0); // не зрозуміло який тим тому що знак питання
+        return variable;
+    }
+
 }
